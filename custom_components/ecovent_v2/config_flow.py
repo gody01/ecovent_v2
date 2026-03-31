d66c3ebd373caca8bc66eef8c186abf31f0b6e75
Andy <145973686+AndyNew2@users.noreply.github.com>
2026-03-31 19:26:53 +0200

    Add update interval configuration option
    Added update interval option to configuration and reconfigure steps.

--- custom_components/ecovent_v2/config_flow.py
+++ custom_components/ecovent_v2/config_flow.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from ecoventv2 import Fan
+from .ecoventv2 import Fan
 import voluptuous as vol
 
 from homeassistant import config_entries, exceptions
@@ -18,7 +18,7 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN
+from .const import DOMAIN, UPDATE_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,6 +30,7 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Optional(CONF_DEVICE_ID, default="DEFAULT_DEVICEID"): str,
         vol.Required(CONF_PASSWORD, default="1111"): str,
         vol.Optional(CONF_NAME, default="Vento Expert Fan"): str,
+        vol.Optional(UPDATE_INTERVAL, default=30): int,
     }
 )
 
@@ -124,6 +125,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         break
                 if user_input[CONF_IP_ADDRESS] == "<broadcast>":
                     raise CannotConnect
+            if user_input[UPDATE_INTERVAL] < 5:
+                    errors[UPDATE_INTERVAL] = "update interval must be at least 5 seconds to prevent overloading the device with requests."
+                    raise exceptions.HomeAssistantError("update interval too low")
 
             info = await validate_input(self.hass, user_input)
             await self.async_set_unique_id(info["id"])
@@ -142,6 +146,74 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfigure step."""
+        errors: dict[str, str] = {}
+
+        # _LOGGER.info("Reconfigure called with user input: %s", user_input)
+        if user_input is not None:
+            try:
+                if user_input[CONF_IP_ADDRESS] == "<broadcast>":
+                    ip = None
+                    ips = self._fan.search_devices("0.0.0.0")
+                    # ips = ["10.94.0.105", "10.94.0.106", "10.94.0.107", "10.94.0.108"]
+                    unique_ids = []
+                    for entry in self._async_current_entries(include_ignore=True):
+                        unique_ids.append(entry.unique_id)
+                    for ip in ips:
+                        self._fan.host = ip
+                        self._fan.id = user_input[CONF_DEVICE_ID]
+                        self._fan.password = user_input[CONF_PASSWORD]
+                        self._fan.name = user_input[CONF_NAME]
+                        self._fan.port = user_input[CONF_PORT]
+                        self._fan.init_device()
+                        if self._fan.id not in unique_ids:
+                            user_input[CONF_IP_ADDRESS] = ip
+                            break
+                    if user_input[CONF_IP_ADDRESS] == "<broadcast>":
+                        raise CannotConnect
+                if user_input[UPDATE_INTERVAL] < 5:
+                        errors[UPDATE_INTERVAL] = "update interval must be at least 5 seconds to prevent overloading the device with requests."
+                        raise exceptions.HomeAssistantError("update interval too low")
+
+                info = await validate_input(self.hass, user_input)
+                # await self.async_set_unique_id(info["id"])
+                # self._abort_if_unique_id_configured()
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_update_reload_and_abort(
+                        self._get_reconfigure_entry(),
+                        data_updates=user_input
+                    )
+
+        ip_configured = self._get_reconfigure_entry().data.get(CONF_IP_ADDRESS,"<broadcast>")
+        port_configured = self._get_reconfigure_entry().data.get(CONF_PORT, 4000)
+        devId_configured = self._get_reconfigure_entry().data.get(CONF_DEVICE_ID, "DEFAULT_DEVICEID")
+        password_configured = self._get_reconfigure_entry().data.get(CONF_PASSWORD, "1111")
+        name_configured = self._get_reconfigure_entry().data.get(CONF_NAME, "Vento Expert Fan")
+        update_interval_configured = self._get_reconfigure_entry().data.get(UPDATE_INTERVAL, 30)
+
+        return self.async_show_form(
+            step_id="reconfigure",data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_IP_ADDRESS, default=ip_configured): str,
+                    vol.Optional(CONF_PORT, default=port_configured): int,
+                    vol.Optional(CONF_DEVICE_ID, default=devId_configured): str,
+                    vol.Required(CONF_PASSWORD, default=password_configured): str,
+                    vol.Optional(CONF_NAME, default=name_configured): str,
+                    vol.Optional(UPDATE_INTERVAL, default=update_interval_configured): int,
+                }
+            ), errors=errors
+        )
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
